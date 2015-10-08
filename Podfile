f1913e1454d3b06207239a6830bab7568e7ae257
Nikita Lutsenko <nlutsenko@me.com>
2015-10-08 11:30:32 -0700

    (っ˘▽˘)っ :cloud: ⊂(◕。◕⊂)

---
+++ Podfile
@@ -0,0 +1,6 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '7.0'
+
+target 'ParseTwitterUtils-Tests', :exclusive => true do
+  pod 'OCMock'
+end
---
+++ Tests/Unit/TwitterUtilsTests.m
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#import "PFTwitterTestCase.h"
+#import "PFTwitterUtils_Private.h"
+#import "PF_Twitter.h"
+
+@import Parse.PFUser;
+@import Parse.PFUserAuthenticationDelegate;
+
+///--------------------------------------
+#pragma mark - Tests
+///--------------------------------------
+
+@interface TwitterUtilsTests : PFTwitterTestCase
+
+@end
+
+@implementation TwitterUtilsTests
+
+///--------------------------------------
+#pragma mark - XCTestCase
+///--------------------------------------
+
+- (void)tearDown {
+    [PFTwitterUtils _setAuthenticationProvider:nil];
+
+    [super tearDown];
+}
+
+///--------------------------------------
+#pragma mark - Tests
+///--------------------------------------
+
+- (void)testInitialize {
+    id userMock = PFStrictClassMock([PFUser class]);
+    OCMExpect(ClassMethod([userMock registerAuthenticationDelegate:[OCMArg checkWithBlock:^BOOL(id obj) {
+        return (obj != nil);
+    }] forAuthType:@"twitter"]));
+
+    [PFTwitterUtils initializeWithConsumerKey:@"a" consumerSecret:@"b"];
+    XCTAssertNotNil([PFTwitterUtils twitter]);
+    XCTAssertEqualObjects([PFTwitterUtils twitter].consumerKey, @"a");
+    XCTAssertEqualObjects([PFTwitterUtils twitter].consumerSecret, @"b");
+
+    OCMVerifyAll(userMock);
+}
+
+- (void)testInitializeTwice {
+    id userMock = PFStrictClassMock([PFUser class]);
+
+    [PFTwitterUtils initializeWithConsumerKey:@"a" consumerSecret:@"b"];
+    XCTAssertNotNil([PFTwitterUtils twitter]);
+    XCTAssertEqualObjects([PFTwitterUtils twitter].consumerKey, @"a");
+    XCTAssertEqualObjects([PFTwitterUtils twitter].consumerSecret, @"b");
+
+    [PFTwitterUtils initializeWithConsumerKey:@"b" consumerSecret:@"c"];
+    XCTAssertNotNil([PFTwitterUtils twitter]);
+    XCTAssertEqualObjects([PFTwitterUtils twitter].consumerKey, @"a");
+    XCTAssertEqualObjects([PFTwitterUtils twitter].consumerSecret, @"b");
+
+    OCMVerifyAll(userMock);
+}
+
+@end
